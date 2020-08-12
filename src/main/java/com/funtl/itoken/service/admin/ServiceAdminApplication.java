01e9593b2c917726ce4461f7947a4d0c7c0e3ed5
test <584571205@qq.com>
2020-08-12 18:17:01 +0800

    init project

---
+++ src/main/java/com/funtl/itoken/service/admin/ServiceAdminApplication.java
@@ -0,0 +1,19 @@
+package com.funtl.itoken.service.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import tk.mybatis.spring.annotation.MapperScan;
+
+/**
+ * @author shkstart
+ * @date 2020/8/11  -  8:15
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@MapperScan("com.funtl.itoken.service.admin.mapper")
+public class ServiceAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceAdminApplication.class,args);
+    }
+}
